0af5411e23c895d6f7b892c490b51123cb7f0810
Lenasaidi <saidilena6@gmail.com>
2026-04-17 18:51:52 +0200

    PostService.ts + CORS Configuration:

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,17 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Post } from '../data/post';
-import { POSTS } from '../data/mock-posts';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class PostService {
+  private apiUrl = `${environment.apiUrl}/v1/posts`;
 
   constructor(private http: HttpClient) {}
 
   getPosts(): Observable<Post[]> {
-    return of(POSTS);
+    return this.http.get<Post[]>(this.apiUrl);
   }
 }
